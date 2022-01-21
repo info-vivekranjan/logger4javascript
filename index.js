5769c82b573bc1f256cb3a441caca7f8821599a9
Vivek Ranjan <infovivek.ranjan@gmail.com>
2022-01-21 15:02:41 +0530

    Version 1.0.0 release of logger4javascript

---
+++ index.js
@@ -0,0 +1,48 @@
+let fs = require("fs");
+
+let Logger = (exports.Logger = {});
+
+const d = new Date();
+
+let filename = `logs/${d.getFullYear()}_${d.getMonth() + 1}_${d.getDate()}.log`;
+
+//Days
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let date = new Date();
+let dayName = days[date.getDay()];
+
+//Time
+
+function formatAMPM(date) {
+  // console.log(date);
+  let strTime = "";
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  strTime = hours + ":" + minutes + " " + ampm;
+  // console.log(strTime);
+  return strTime;
+}
+
+Logger.info = function (msg) {
+  let messageLog = `${dayName} ${d.getFullYear()}-${
+    d.getMonth() + 1
+  }-${d.getDate()} - ${formatAMPM(new Date())}`;
+
+  fs.exists(filename, (exists) => {
+    if (exists) {
+      let message = messageLog + " : " + msg + "\n";
+      let infoStream = fs.createWriteStream(filename, { flags: "a" });
+      infoStream.write(message);
+    } else {
+      let message = messageLog + " : " + msg + "\n";
+      let infoStream = fs.createWriteStream(filename);
+      infoStream.write(message);
+    }
+  });
+};
+module.exports = Logger
